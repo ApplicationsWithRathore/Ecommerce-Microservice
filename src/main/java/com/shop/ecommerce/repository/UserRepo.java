80b63c6a5f22b51cf905a427bf5e11c176dbcdc3
ApplicationsWithRathore <jitendrasinghrathore74885@gmail.com>
2023-12-28 11:25:49 +0530

    updated

--- src/main/java/com/shop/ecommerce/repository/UserRepo.java
+++ src/main/java/com/shop/ecommerce/repository/UserRepo.java
@@ -1,5 +1,10 @@
 package com.shop.ecommerce.repository;
 
-public class UserRepo {
+import com.shop.ecommerce.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
 
 }
